d5966230197c31bf471088593d871abe384cd9d2
Prince Parteti <princeparteti99@gmail.com>
2025-04-06 15:23:56 +0530

    Update MobileMenu.jsx

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -37,7 +37,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                     </a>
 
                     <a 
-                    href="#projects" 
+                    href="#project" 
                     onClick={() => setMenuOpen(false)} 
                     className={`text-2xl font-semibold text-white my-4 tranform transition-transform duration-300
                         ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-5"}
